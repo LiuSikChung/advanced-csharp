6ea67e92426ec44e87e3ec2de3759c4f44ba693d
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-06-30 14:06:31 +0300

    AsynchronousProgramming - modified the main function to execute the example class

--- AdvancedCSharp/AsynchronousProgramming/Program.cs
+++ AdvancedCSharp/AsynchronousProgramming/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AsynchronousProgramming.EventArgs;
+using System;
+using System.Threading;
 
 namespace AsynchronousProgramming
 {
     class Program
     {
+        public static bool isFinished = false;
+
         static void Main(string[] args)
         {
+            var primeNumberCalculator = new PrimeNumberCalculator();
+            primeNumberCalculator.CalculatePrimeCompleted += PrimeNumberCalculator_CalculatePrimeCompleted;
+            primeNumberCalculator.ProgressChanged += PrimeNumberCalculator_ProgressChanged;
+
+            primeNumberCalculator.CalculatePrimeAsync(1234567, Guid.NewGuid());
+
+            // thread synchronization
+            while (isFinished == false);
+
+            Console.WriteLine("Main finished !!");
+        }
+
+        private static void PrimeNumberCalculator_ProgressChanged(System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            
+        }
+
+        private static void PrimeNumberCalculator_CalculatePrimeCompleted(object sender, CalculatePrimeCompletedEventArgs e)
+        {
+            Console.WriteLine(e.IsPrime);
+
+            isFinished = true;
         }
     }
 }
