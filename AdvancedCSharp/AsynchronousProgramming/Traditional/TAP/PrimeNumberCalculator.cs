5e04384f54c04871eb2f3a2b3644eb29fcd08df6
laurentiustamate94 <laurentiustamate94@yahoo.com>
2016-08-06 23:34:35 +0300

    Add TAP rewriting of PrimeNumberCalculator example

---
+++ AdvancedCSharp/AsynchronousProgramming/Traditional/TAP/PrimeNumberCalculator.cs
@@ -0,0 +1,83 @@
+﻿using AsynchronousProgramming.Traditional.EventArgs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming.Modern
+{
+    public class PrimeNumberCalculator
+    {
+        private volatile int LastPrimeNumber = 100;
+
+        public Task<bool> CalculatePrimeAsync(int numberToTest)
+        {
+            var firstDivisor = 1;
+
+            var task = Task
+                .FromResult(BuildPrimeNumberList(numberToTest))
+                .ContinueWith((prevTask) => IsPrime(prevTask.Result, numberToTest, out firstDivisor));
+
+            return task;
+        }
+
+        private bool IsPrime(ArrayList primes, int n, out int firstDivisor)
+        {
+            var foundDivisor = false;
+            var exceedsSquareRoot = false;
+
+            var i = 0;
+            var divisor = 0;
+            firstDivisor = 1;
+
+            while ((i < primes.Count) && !foundDivisor && !exceedsSquareRoot)
+            {
+                divisor = (int)primes[i++];
+
+                if (divisor * divisor > n)
+                {
+                    exceedsSquareRoot = true;
+                }
+                else if (n % divisor == 0)
+                {
+                    firstDivisor = divisor;
+                    foundDivisor = true;
+                }
+            }
+
+            return !foundDivisor;
+        }
+
+        private ArrayList BuildPrimeNumberList(int numberToTest)
+        {
+            var primes = new ArrayList();
+            var firstDivisor = 1;
+            var n = 5;
+
+            primes.Add(2);
+            primes.Add(3);
+
+            while (n < numberToTest)
+            {
+                if (IsPrime(primes, n, out firstDivisor))
+                {
+                    var progressPercentage = (int)((float)n / (float)numberToTest * 100);
+
+                    //LastPrimeNumber = n;
+
+                    Console.WriteLine("Progress: {0}%", progressPercentage);
+
+                    primes.Add(n);
+                }
+
+                // Skip even numbers.
+                n += 2;
+            }
+
+            return primes;
+        }
+    }
+}
